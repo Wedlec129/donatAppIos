12427a298e34d792b72a6bbc771b513e92997134
Wedlec129 <boryasokolov@gmail.com>
2023-09-17 13:30:58 +0300

    Initial Commit

---
+++ donatApp/donatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  donatAppApp.swift
+//  donatApp
+//
+//  Created by Борух Соколов on 17.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct donatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
